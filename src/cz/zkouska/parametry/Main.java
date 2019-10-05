5dfc78af009e156628969e3df75e7df83801433b
Martin Žamberský <martin.zambersky@gmail.com>
2019-10-05 16:36:30 +0200

    Initial commit

---
+++ src/cz/zkouska/parametry/Main.java
@@ -0,0 +1,34 @@
+package cz.zkouska.parametry;
+
+public class Main {
+
+    public static void main (String args[]){
+
+        int x = 1;
+        System.out.println("Promenna x pred provolanim prictiJednicku(x): " + x);
+        prictiJednicku(x);
+        System.out.println("Promenna x po provolani prictiJednicku(x): " + x);
+
+        System.out.println("=====================");
+
+        Trida trida = new Trida(5);
+        System.out.println("Field trida.cislo pred provolanim prictiJednickuKFieldu: " + trida.getCislo());
+        prictiJednickuKFieldu(trida);
+        System.out.println("Field trida.cislo po provolani prictiJednickuKFieldu: " + trida.getCislo());
+
+    }
+
+
+    // primitivni datovy typ - pri volani se HODNOTA zkopiruje do parametru metody int a
+    public static void prictiJednicku(int a){
+        System.out.println("Metoda prictiJednicku, predano bylo: " + a);
+        a += 1;
+        System.out.println("Metoda prictiJednicku, vysledek po pricteni: " + a);
+    }
+
+    // preda se ODKAZ na objekt
+    public static void prictiJednickuKFieldu(Trida a){
+        a.setCislo(a.getCislo() + 1);
+        System.out.println("Metoda prictiJednickuKFieldu, vysledek po pricteni: " + a.getCislo());
+    }
+}
---
+++ src/cz/zkouska/parametry/Trida.java
@@ -0,0 +1,17 @@
+package cz.zkouska.parametry;
+
+public class Trida {
+    private int cislo;
+
+    public int getCislo() {
+        return cislo;
+    }
+
+    public void setCislo(int cislo) {
+        this.cislo = cislo;
+    }
+
+    public Trida(int cislo) {
+        this.cislo = cislo;
+    }
+}
